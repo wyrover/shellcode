ec2291c9782a082a29e3b7568a71aaadbbeece74
odzhan <odzhan@protonmail.com>
2017-06-16 10:05:36 +0100

    Update connect.asm

--- win/connect.asm
+++ win/connect.asm
@@ -238,7 +238,9 @@ gapi_l0x:
     db     65h              ; gs:
     dec    eax
     mov    edi, [edx]
+    dec    eax
     mov    edi, [edi+24]
+    dec    eax
     mov    edi, [edi+16]
     jmp    gapi_l2x
 
